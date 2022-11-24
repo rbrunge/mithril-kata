0b91041ca6d516b7b668101f70a32042d998df23
Robert Runge <rru@immeo.dk>
2022-11-24 17:52:10 +0100

    Kata_030_component_using_babel updated webpack to semver major 5

--- Kata_030_component_using_babel/src/MyComponent.js
+++ Kata_030_component_using_babel/src/MyComponent.js
@@ -3,7 +3,7 @@ import m from 'mithril';
 const MyComponent = {
     view: () => {
         return m("main", [
-            m("h1", { class: "title" }, "Kata: component using babel"),
+            m("h2", { class: "title" }, "Kata: component using babel"),
             m("p", "Just some text"),
         ])
     }
